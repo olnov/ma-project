00e5d5acb82c395957a6bc054dae6d74dccbc922
Robert McIsaac <robmcisaac@hotmail.co.uk>
2025-05-21 10:03:56 +0100

    slight edit to main.jsx - adding scope for future use

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -10,6 +10,7 @@ root.render(
 <Auth0Provider
   domain={import.meta.env.VITE_AUTH0_DOMAIN}
   clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+  // scope="openid profile email"
   authorizationParams={{
     redirect_uri: window.location.origin,
     audience: import.meta.env.VITE_AUTH0_AUDIENCE,

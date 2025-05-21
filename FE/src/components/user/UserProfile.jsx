b57d6800dec6071016dabfa7cfe09635f5e7616c
Robert McIsaac <robmcisaac@hotmail.co.uk>
2025-05-21 09:56:22 +0100

    updated userprofile, added react dependency

--- FE/src/components/user/UserProfile.jsx
+++ FE/src/components/user/UserProfile.jsx
@@ -1,16 +1,40 @@
-import { useAuth0 } from '@auth0/auth0-react'
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
 
-const UserProfile = () => {
-  const { user, isAuthenticated } = useAuth0()
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-  if (!isAuthenticated) return null
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
 
   return (
-    <div>
-      <p>Logged in as: {user.name}</p>
-      <p>Email: {user.email}</p>
-    </div>
-  )
-}
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+};
 
-export default UserProfile
+export default Profile;
+
+
+// import { useAuth0 } from '@auth0/auth0-react'
+
+// const UserProfile = () => {
+//   const { user, isAuthenticated } = useAuth0()
+
+//   if (!isAuthenticated) return null
+
+//   return (
+//     <div>
+//       <p>Logged in as: {user.name}</p>
+//       <p>Email: {user.email}</p>
+//     </div>
+//   )
+// }
+
+// export default UserProfile

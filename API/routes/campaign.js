bfdcf55adf9da6329961ad638fcf1eb0099efed2
Oleg Novikov <o.novikov@ymail.com>
2025-06-10 10:21:47 +0100

    chores: fixed typos in file names

--- API/routes/campaign.js
+++ API/routes/campaign.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createNewCampaign, getCampaignsByUserId } = require('../controllers/campaign.constoller');
+const { createNewCampaign, getCampaignsByUserId } = require('../controllers/campaign.controller');
 
 router.post('/', createNewCampaign);
 router.get('/:userId', getCampaignsByUserId);

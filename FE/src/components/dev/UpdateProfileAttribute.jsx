cd6bf50072c7659c0fb73d9e436481a767b02a41
Robert McIsaac <robmcisaac@hotmail.co.uk>
2025-06-12 09:03:34 +0100

    feat(user-service): add updateUserProfile  helper for user updates

---
+++ FE/src/components/dev/UpdateProfileAttribute.jsx
@@ -0,0 +1,10 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
+const UpdateProfileAttribute = () => {
+    const { user } = useAuth0();
+
+
+}
+
+export default UpdateProfileAttribute;

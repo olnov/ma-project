55a99c77510b9ce1932156504d4324c4d96447d9
Robert McIsaac <robmcisaac@hotmail.co.uk>
2025-06-19 14:22:12 +0100

    feat(auth): add checkJwt middleware to API controllers
    - imported `checkJwt` in each route controller under `/API`
    - applied middleware to secure protected endpoints before business logic

--- API/routes/campaign.js
+++ API/routes/campaign.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const checkJwt = require("../middleware/checkJwt");
 const { createNewCampaign, getCampaignsByUserId, getCampaignByIdController } = require('../controllers/campaign.controller');
 
-router.post('/', createNewCampaign);
-router.get('/user/:userId', getCampaignsByUserId);
-router.get('/campaign/:campaignId', getCampaignByIdController);
+router.post('/', checkJwt, createNewCampaign);
+router.get('/user/:userId', checkJwt, getCampaignsByUserId);
+router.get('/campaign/:campaignId', checkJwt, getCampaignByIdController);
 
 module.exports = router;

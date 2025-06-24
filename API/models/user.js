090ca299acf7781b31c8c53130dc5a90d3393524
Robert McIsaac <robmcisaac@hotmail.co.uk>
2025-06-24 09:31:03 +0100

    feat(model): add validation rules to User model

--- API/models/user.js
+++ API/models/user.js
@@ -3,11 +3,20 @@ const { Schema, Types} = mongoose;
 
 const UserSchema = new Schema(
     {
-        auth0Id: { type: String, required: true, unique: true }, //Auth0 sub
+        auth0Id: {
+            type: String,
+            required: [true, "Auth0 sub is required"],
+            unique: true 
+        }, //Auth0 sub
         username: { type: String, unique: true },
         firstName: { type: String},
         lastName: { type: String},
-        email: { type: String, required: true, unique: true }
+        email: {
+            type: String,
+            required: true,
+            unique: [true, "Email is required"],
+            match: [/^[^@\s]+@[^@\s]+\.[^@\s]+$/, "Must be a valid email."]
+        }
     },
     {
         timestamps: true //for createdAt and updatedAt
